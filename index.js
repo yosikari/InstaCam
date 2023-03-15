0a6ef3f8e6b4a7a061f41d7a2b049f98c95d84bf
yosikari <yosikari@gmail.com>
2023-03-15 04:40:00 +0200

    added javascript to open notifications and msgs

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+//SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item')
+
+// MESSAGES
+const messagesNotification = document.querySelector('#messages-notification')
+const messages = document.querySelector('.messages')
+
+
+//remove active class for all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active')
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem()
+        item.classList.add('active')
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+                style.display = 'none'
+        } else {
+            document.querySelector('.notifications-popup').
+                style.display = 'block'
+            document.querySelector('#notifications .notification-count').
+                style.display = 'none'
+        }
+    })
+})
+
+// MESSAGES
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)'
+    messagesNotification.querySelector('.notification-count').
+        style.display = 'none'
+    setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000)
+})
+
