d8ad6800ffffb1616cf8e8686cf948d0381a7ea7
yosikari <yosikari@gmail.com>
2023-03-15 13:09:54 +0200

    added chat msgs filter

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const menuItems = document.querySelectorAll('.menu-item')
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification')
 const messages = document.querySelector('.messages')
-
+const message = messages.querySelectorAll('.message')
+const messageSearch = document.querySelector('#message-search')
 
 //remove active class for all menu items
 const changeActiveItem = () => {
@@ -32,6 +33,23 @@ menuItems.forEach(item => {
 })
 
 // MESSAGES
+//searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase()
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase()
+        if (name.indexOf(val) != -1) {
+            user.style.display = 'flex'
+        } else {
+            user.style.display = 'none'
+        }
+    })
+}
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage)
+
+//highlight messages card when click messages menu btn
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)'
     messagesNotification.querySelector('.notification-count').
@@ -40,4 +58,7 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none'
     }, 2000)
 })
+
+// THEME DISPLAY CUSTOMIZATION
+
 
